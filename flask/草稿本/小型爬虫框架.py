d508551644046f16c7e77401ee2b3965254818f4
cza <chenziangsg@163.com>
2019-10-17 23:50:54 +0800

    flask源码学习

--- flask/草稿本/小型爬虫框架.py
+++ flask/草稿本/小型爬虫框架.py
@@ -10,3 +10,18 @@ class Base:
     def parse(self, response):
         """Ni"""
 
+import asyncio
+import time
+
+async def test1():
+    time.sleep(5)
+    print('test1')
+async def test2():
+    time.sleep(2)
+    print('test2')
+async def main():
+    await asyncio.gather(test1(), test2())
+import time
+now = time.time()
+asyncio.run(main())
+print(time.time()-now)
