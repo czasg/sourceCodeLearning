85fe8ee4897e2a0c030ceeffd9d6cf58232b360d
cza <czasg0.0>
2019-10-25 16:50:34 +0800

    learning

--- asyncio/学习/1.py
+++ asyncio/学习/1.py
@@ -1,12 +1,26 @@
 import threading
 import asyncio
+import socket
+import selectors
+import socketserver
+
 loop = asyncio.new_event_loop()
+selector = selectors.DefaultSelector()
+
+
+def selector_loop():
+    while True:
+        for sock, mask in selector.select():
+            pass
 
 
 class _RunningLoop(threading.local):
     loop_pid = (None, None)
 
+
 _running_loop = _RunningLoop()
 
 if __name__ == '__main__':
+    sock = socket.socket()
     print(_running_loop.loop_pid)
+    selector_loop()
