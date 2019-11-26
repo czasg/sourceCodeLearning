33362fb30f727ff424ab0a96689228f7e5368896
cza <chenziangsg@163.com>
2019-11-26 21:28:59 +0800

    update

--- flask/学习/1.py
+++ flask/学习/1.py
@@ -1,4 +1,8 @@
-from flask import Flask, jsonify, url_for, request
+from flask import Flask, \
+    jsonify, \
+    url_for, \
+    request, \
+    render_template
 
 app = Flask(__name__)
 
@@ -7,18 +11,25 @@ app = Flask(__name__)
 def test():
     return "Hello World!"
 
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template("hello.html", name=(name or "young man"))
+
 
 @app.route('/news/classify', methods=["POST", "GET"])
 def news_classify():
     if request.method == "POST":
         return "This is a News Classify"
     if request.method == "GET":
-        pass
+        return "This is a News Classify"
 
 
 if __name__ == '__main__':
     # app.run()
 
-    with app.test_request_context():
+    with app.test_request_context('/hello', method='GET'):  # 单元测试
+        print(request.path, request.method)
         print(url_for("test", next="cza"))
         print(url_for("news_classify"))
+        print(url_for("static", filename="style.css"))
