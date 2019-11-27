77ea966622195244190cd42bbead6814a22eb899
cza <czasg0.0>
2019-11-27 12:11:48 +0800

    update

--- werkzeug/学习/1.py
+++ werkzeug/学习/1.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from werkzeug.wrappers import Request, Response
-def application(environ, start_response):
+def application_cza(environ, start_response):
     response = Response('Hello World!', mimetype='text/plain')
     return response(environ, start_response)
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple("127.0.0.1", 5000, application, use_reloader=True)
+    run_simple("127.0.0.1", 5000, application_cza, use_reloader=True)
 
 #
 # import os
