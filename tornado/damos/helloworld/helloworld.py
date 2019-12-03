a1cca974197fa518c7361ad533432ea6f2659a73
cza <czasg0.0>
2019-12-03 09:57:23 +0800

    update tornado demo

--- tornado/damos/helloworld/helloworld.py
+++ tornado/damos/helloworld/helloworld.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
+# -*- coding: utf-8 -*-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -24,17 +10,19 @@ from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
 
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):  # 感觉还是函数式编程友好一点啊，看着就舒服. 但是这种也感觉不错，针对各种请求有不同的方案清洗明了
     def get(self):
         self.write("Hello, world")
 
 
 def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([(r"/", MainHandler)])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+    # tornado.options.parse_command_line()
+    application = tornado.web.Application(
+        [(r"/", MainHandler)],  # url的映射关系. 后面其实还可以接一些初始化参数
+    )
+    http_server = tornado.httpserver.HTTPServer(application)  # 这是一种启动方式, 调用HttPServer
+    http_server.listen(options.port)  # 监听端口
+    tornado.ioloop.IOLoop.current().start()  # 启动异步轮询
 
 
 if __name__ == "__main__":
