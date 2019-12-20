0e122319e46c3d68fb305461b9c4557d7cf5b97e
cza <czasg0.0>
2019-12-20 16:53:56 +0800

    update note

---
+++ flask/学习/temp.py
@@ -0,0 +1,53 @@
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+bp = Blueprint("bp", __name__)
+
+
+@app.route("/")
+def index():
+    return "hei"
+
+
+@bp.route("/")
+def test():
+    return "test"
+
+
+@app.route("/mul/ha/hi")
+def mul():
+    # return "cza", 200, ("Content-Type","text/html")
+    return "cza", 200
+
+
+@bp.before_request  # app.before_request_funcs[self.name].append
+def bp_req():
+    pass
+
+
+@app.before_request
+def ha():
+    pass
+
+
+@app.url_value_preprocessor
+def funcfunc(endpoint, view_args):
+    # print(endpoint, view_args)
+    ...
+
+
+if __name__ == '__main__':
+    app.register_blueprint(bp, url_prefix="/bp")  # will register func into flask-app
+    app.run(port=8888)
+
+    # from werkzeug.routing import Map, Rule
+    #
+    # map = Map([
+    #     Rule('/', endpoint='index'),
+    #     Rule('/downloads', endpoint='downloads/index'),
+    #     Rule('/downloads/<int:id>', endpoint='downloads/show')
+    # ])
+    # urls = map.bind("example.com", "/")
+    # print(urls.match("/", "GET"))
+    # print(urls.match("/downloads"))
+    # print(urls.match("/downloads/42"))
