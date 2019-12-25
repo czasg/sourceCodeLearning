e015b6a946666e2d60feffba9ce96d78a2b8a7e0
cza <chenziangsg@163.com>
2019-12-25 20:46:55 +0800

    update

--- celery/学习/别人的/1/app.py
+++ celery/学习/别人的/1/app.py
@@ -3,7 +3,7 @@ import random
 import time
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
 from celery import Celery
 
 
@@ -24,22 +24,22 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 
 # Initialize extensions
-mail = Mail(app)
+# mail = Mail(app)
 
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 
-@celery.task
-def send_async_email(email_data):
-    """Background task to send an email with Flask-Mail."""
-    msg = Message(email_data['subject'],
-                  sender=app.config['MAIL_DEFAULT_SENDER'],
-                  recipients=[email_data['to']])
-    msg.body = email_data['body']
-    with app.app_context():
-        mail.send(msg)
+# @celery.task
+# def send_async_email(email_data):
+#     """Background task to send an email with Flask-Mail."""
+#     msg = Message(email_data['subject'],
+#                   sender=app.config['MAIL_DEFAULT_SENDER'],
+#                   recipients=[email_data['to']])
+#     msg.body = email_data['body']
+#     with app.app_context():
+#         mail.send(msg)
 
 
 @celery.task(bind=True)
@@ -78,11 +78,11 @@ def index():
     }
     if request.form['submit'] == 'Send':
         # send right away
-        send_async_email.delay(email_data)
+        # send_async_email.delay(email_data)
         flash('Sending email to {0}'.format(email))
     else:
         # send in one minute
-        send_async_email.apply_async(args=[email_data], countdown=60)
+        # send_async_email.apply_async(args=[email_data], countdown=60)
         flash('An email will be sent to {0} in one minute'.format(email))
 
     return redirect(url_for('index'))
