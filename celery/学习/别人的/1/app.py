94ffbe6005e84487b68dec06f7f286067a48269a
cza <czasg0.0>
2019-12-26 11:26:19 +0800

    update

--- celery/学习/别人的/1/app.py
+++ celery/学习/别人的/1/app.py
@@ -3,7 +3,7 @@ import random
 import time
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
-# from flask_mail import Mail, Message
+from flask_mail import Mail, Message
 from celery import Celery
 
 
@@ -11,12 +11,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+# app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
 
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
@@ -125,4 +125,4 @@ def taskstatus(task_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
