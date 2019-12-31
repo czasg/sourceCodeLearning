80150b4123e50662871af2ade37fd46085d90f78
cza <czasg0.0>
2019-12-31 16:57:33 +0800

    update

--- apscheduler/学习/3强大的cron表达.py
+++ apscheduler/学习/3强大的cron表达.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
+
 """
 class apscheduler.triggers.cron.CronTrigger(
 year=None, 
@@ -19,9 +20,13 @@ jitter=None)
 
 sch = BlockingScheduler()
 bac = BackgroundScheduler()
+
+
 def my_job(): print("hello world")
 
-sch.add_job(my_job, 'cron', month='1-6,6-12', day='1-30', hour='0-24')
+
+# sch.add_job(my_job, 'cron', month='1-6,6-12', day='1-30', hour='0-24')
+# sch.start()
+
+sch.add_job(my_job, 'cron', second="55")
 sch.start()
-
-
